3d561e2524ae2e84fb4662a6e1074635a16c2634
watcharapon.hon <watcharapon.hon@itintrend.co.th>
2026-05-07 10:13:11 +0700

    [ADD] install

---
+++ install.sh
@@ -0,0 +1,3 @@
+curl -fsSL https://raw.githubusercontent.com/watcharapon/traefik-domains/refs/heads/main/traefik-domains \
+| sudo tee /usr/local/bin/traefik-domains > /dev/null \
+&& sudo chmod +x /usr/local/bin/traefik-domains
