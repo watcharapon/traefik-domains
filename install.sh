0313f98138de5f07a2e84f4b4d54bd18dda00d92
watcharapon.hon <watcharapon.hon@itintrend.co.th>
2026-05-07 10:15:24 +0700

    make executable

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 curl -fsSL https://raw.githubusercontent.com/watcharapon/traefik-domains/refs/heads/main/traefik-domains \
 | sudo tee /usr/local/bin/traefik-domains > /dev/null \
 && sudo chmod +x /usr/local/bin/traefik-domains
